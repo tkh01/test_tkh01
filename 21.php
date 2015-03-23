67595bc0c344c471336a1e1cb57b95eab1828435
Tae <tkh01@naver.com>
2015-03-23 16:55:25 +0900

    create home

---
+++ 21.php
@@ -0,0 +1,11 @@
+<?php
+if(
+    ($_POST['id'] === 'tae' or $_POST['id'] === 'k8805' or $_POST['id'] === 'sorialgi')
+    and
+    $_POST['password'] === 'xormsgud01'
+){
+    echo 'right';
+} else {
+    echo 'wrong';
+}
+?>
